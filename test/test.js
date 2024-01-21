a99c21bc20ac9f9ca625d05ca78861ffab3cf319
Kamrul Hasan <kamrulhasan20656@gmail.com>
2024-01-21 23:00:32 +0600

    create some user and tickets

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const myDb = require('../db/db');
+
+myDb.create('user 1',10);
+myDb.create('user 2',10);
+myDb.create('user 3',10);
+myDb.create('user 4',10);
+myDb.create('user 5',10);
+myDb.create('user 6',10);
+myDb.create('user 7',10);
+myDb.create('user 8',10);
